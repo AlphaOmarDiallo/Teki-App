b0d6abf176c4a56173d9ea6b2852c959455cd7df
Alpha <adiallo@exomind.fr>
2024-12-09 12:40:58 +0100

    Initial Commit

---
+++ Teki/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Teki
+//
+//  Created by Alpha on 09/12/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TekiTests/TekiTests.swift
@@ -0,0 +1,17 @@
+//
+//  TekiTests.swift
+//  TekiTests
+//
+//  Created by Alpha on 09/12/2024.
+//
+
+import Testing
+@testable import Teki
+
+struct TekiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
